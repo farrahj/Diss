c2bdda905bf10bc14587872cc5227402b60429cf
farrahj <J.farrah@btinternet.com>
2019-04-25 17:24:29 +0100

    Progress

--- script2.py
+++ script2.py
@@ -1,29 +1,64 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import socketserver
 import json
+import sys
+
+USERNAME = "james"
+PASSWORD = "testingPassword"
+
+def capitalLetter(password):
+    #Do checks for capital
+    #if there is a capital, return False
+    
+    #return True (if it doesnt satisfy this function)
+
+def bruteforce(password, main):
+    flag = 0
+    print("ENTERED PASSWORD: " + password)
+    if (capitalLetter(password) == True):
+        flag += 1
+    if ....
+        flag += 1
+
+    
+
+
+
+
 
 def controller(data, main):
     # try:
-    #     data = json.loads(data)
+    data = json.loads(data)
+    if ("username" in data and "password" in data):
+        if (data["username"] == USERNAME):
+            if (data["password"] == PASSWORD):
+                main._set_response(200, "correct information")
+            else:
+                print("BRUTEFORCE CHECK")
+                bruteforce(data["password"], main)
+        else:
+            main._set_response(403, "invalid credentials")
+    else:
+        main._set_response(400, "JSON must include a username and password")
     # except:
-    #     print("")
-    main._set_headers()
+    #     main._set_response(400, "invalid data")
 
 class S(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/html')
+    def _set_response(self, code = 200, message = ""):
+        self.send_response(code)
+        self.send_header('Content-Type', 'application/json')
         self.end_headers()
+        json_message = json.dumps({'message': message}).encode()
+        self.wfile.write(json_message)
     
     def do_GET(self):
-        self._set_headers()
-        print("GET REQUEST PERFORMED")
+        self._set_response(404, "route not found")
 
     def do_POST(self):
-        # #doesn't do anything with posted data
-        # self._set_headers()
-        #Get the content length so we know how much data to read.
-        content_length = int(self.headers['Content-Length'])
+        if (self.headers['Content-Length'] != None):
+            content_length = int(self.headers['Content-Length'])
+        else:
+            self._set_response(400, "JSON must include a username and password")
         #Read the post data
         post_data = self.rfile.read(content_length).decode("utf-8")
         controller(post_data, self)
@@ -41,3 +76,6 @@ if __name__ == "__main__":
         run(port=int(argv[1]))
     else:
         run()
+
+
+# curl -i -X POST 127.0.0.1:8080 -d '{"username": "james", "password": "testingPasord"}'
